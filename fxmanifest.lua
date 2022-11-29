d1d41b94714909338ca392dbccb3d50251638d2c
marciogalvao89 <32371159+marciogalvao89@users.noreply.github.com>
2022-11-29 02:13:41 +0000

    Carros upload

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -28,3 +28,17 @@ server_scripts {
 }
 
 lua54 'yes'
+
+files {
+    '**/**/vehiclelayouts.meta',
+    '**/**/vehicles.meta',
+    '**/**/carvariations.meta',
+    '**/**/handling.meta',
+    '**/**/carcols.meta',
+}
+
+data_file 'VEHICLE_LAYOUTS_FILE' '**/**/vehiclelayouts.meta'
+data_file 'HANDLING_FILE' '**/**/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' '**/**/vehicles.meta'
+data_file 'CARCOLS_FILE' '**/**/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' '**/**/carvariations.meta'
